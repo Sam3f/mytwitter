6af737163ff8f551bb6dfabc596946b1c5245ce6
sam3f <samac@udel.edu>
2022-10-24 00:40:09 -0400

    commitv5

--- components/Feed.js
+++ components/Feed.js
@@ -28,12 +28,6 @@ function Feed() {
     >
       <div className="text-[#d9d9d9] flex items-center sm:justify-between py-2 px-3 sticky top-0 z-50 bg-gray-900 border-b border-gray-700">
         <h2 className="text-lg sm:text-xl font-bold">Home</h2>
-        <div
-          className="hoverAnimation w-9 h-9 flex items-center 
-        justify-center xl:px-0 ml-auto"
-        >
-          <SparklesIcon className="h-5 text-white"></SparklesIcon>
-        </div>
       </div>
 
       <Input></Input>
